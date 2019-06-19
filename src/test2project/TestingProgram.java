ce161c723a6389e0b7ac175671b293976269988b
Akshay Verma <akshay.verma@roposo.com>
2019-06-19 12:43:09 +0530

    dark

--- src/test2project/TestingProgram.java
+++ src/test2project/TestingProgram.java
@@ -7,5 +7,6 @@ public static void main(String []args) {
 		System.out.println("ok");
 		System.out.println("if i wanted more?");
 		System.out.println("light");
+		System.out.println("dark");
 	}
 }
